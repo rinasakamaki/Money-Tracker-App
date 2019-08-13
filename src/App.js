390e723b82c49d2327fa83a48df4b73b55fec115
rinasakamaki <h0508g@gmail.com>
2019-08-13 22:32:26 +0900

    got the GET request working from frontend side to DB

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import icon from "./favicon.ico";
 import Form from "./Form";
+import Today from "./Today";
 
 class App extends Component {
   state = {
@@ -18,6 +19,7 @@ class App extends Component {
         <span id="AppTitle">Money Tracker App</span>
         <Form onSubmitParent={fields => this.onSubmitParent(fields)} />
         <p>{JSON.stringify(this.state.fields, null, 2)}</p>
+        <Today />
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Calendar from "./Calendar.jsx";
+import axios from "axios";
 
 export default class Form extends React.Component {
   state = {
@@ -16,9 +16,18 @@ export default class Form extends React.Component {
     });
   };
 
+  getHistory = async () => {
+    const { data: history } = await axios.get("/api/history");
+    console.log("history:", history);
+    return history;
+  };
+
   onSubmit = e => {
     e.preventDefault();
     this.props.onSubmitParent(this.state);
+    const array = this.getHistory().then(array => {
+      console.log("array:", array);
+    });
     this.setState({
       name: "",
       cost: "",
